768c24f68848807b4e3331c68064bfb78b7784d4
RuneTable <admin@runetable.app>
2025-04-18 10:47:43 -0400

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:3001'
-    }
-  }
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
 });
